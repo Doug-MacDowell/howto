2226ac58af82a72a022a0928f469bb65f4cd9ece
Doug MacDowell <dougmac9@uw.edu>
2014-02-27 12:47:46 -0800

    added a list model and associated files

---
+++ app/models/list.rb
@@ -0,0 +1,3 @@
+class List < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 3 }
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Howtolist::Application.config.session_store :cookie_store, key: '_howtolist_session'
